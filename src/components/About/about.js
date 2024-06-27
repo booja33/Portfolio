d844437688d24a3e48c7405887e059467b243f40
booja33 <boojasree03@gmail.com>
2024-06-27 15:03:09 +0530

    Changes

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,46 +1,30 @@
-import React, { useState } from 'react';
-import './about.css'; // Import the CSS file for styles
+import React from 'react';
+import './about.css';
 
 const About = () => {
-  const [userData] = useState({
-    name: 'Booja',
-    dateOfBirth: 'March 15, 2003',
-    phoneNumber: '6381858023',
-    address: 'Nagercoil'
-  });
-
-  return (
-    <div className="about-container">
-      <div className="about-content">
-        <h1>About Me</h1>
-        
-        <div className="user-info">
-          <div className="detail">
-            <p><strong>Name:</strong></p>
-            <p>{userData.name}</p>
-          </div>
-          <div className="detail">
-            <p><strong>Date of Birth:</strong></p>
-            <p>{userData.dateOfBirth}</p>
-          </div>
-          <div className="detail">
-            <p><strong>Phone Number:</strong></p>
-            <p>{userData.phoneNumber}</p>
-          </div>
-          <div className="detail">
-            <p><strong>Address:</strong></p>
-            <p>{userData.address}</p>
-          </div>
+    return (
+        <div className="about-me">
+            <h2>About Me</h2>
+            <p>
+             Welcome to my profile. I believe in creating user-centric designs that blend aesthetics with functionality, striving to elevate brands and enhance user experiences online.
+            </p>
+            <div className="info-box">
+                <div className="info">
+                    <div className="info-left">
+                        <p><strong>Name:</strong> Booja Sree</p>
+                        <p><strong>Email:</strong>boojasree03@gmail.com</p>
+                        <p><strong>Phone:</strong> 6381858023</p>
+                    </div>
+                    <div className="info-right">
+                        <p><strong>Address:</strong> Nagercoil</p>
+                        <p><strong>Degree:</strong> BTech</p>
+                        
+                    </div>
+                </div>
+            </div>
+            <button className="download-btn">Download CV</button>
         </div>
-        
-        <p>
-          Hi, I'm {userData.name}! Welcome to my profile. I believe in creating user-centric designs that blend aesthetics with functionality, striving to elevate brands and enhance user experiences online.
-
-
-        </p>
-      </div>
-    </div>
-  );
-};
+    );
+}
 
 export default About;

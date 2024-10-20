9ef3ac4abbc26f4be5b4d0b05caa21eaf69914d8
Booja Sree <boojasree03@gmail.com>
2024-10-20 09:26:35 +0530

    changes

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './about.css';
+import CV from '../../assets/resume.pdf';
 
 const About = () => {
     return (
@@ -12,19 +13,21 @@ const About = () => {
                 <div className="info">
                     <div className="info-left">
                         <p><strong>Name:</strong> Booja Sree</p>
-                        <p><strong>Email:</strong>boojasree03@gmail.com</p>
+                        <p><strong>Email:</strong> boojasree03@gmail.com</p>
                         <p><strong>Phone:</strong> 6381858023</p>
                         <p><strong>DOB:</strong> 15-03-2003</p>
                     </div>
                     <div className="info-right">
-                    <p><strong>Age:</strong> 21</p>
+                        <p><strong>Age:</strong> 21</p>
                         <p><strong>Address:</strong> Nagercoil</p>
                         <p><strong>Degree:</strong> BTech</p>
-                        
                     </div>
                 </div>
             </div>
-            <button className="download-btn">Download CV</button>
+            {/* Wrap the button in the anchor tag */}
+            <a href={CV} download="Booja_Sree_Resume.pdf" aria-label="Download Booja Sree's Resume">
+                <button className="download-btn">Download Resume</button>
+            </a>
         </div>
     );
 }
